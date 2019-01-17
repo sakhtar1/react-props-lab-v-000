e36a635662a34b0e7ebc9aa92770840d9119dc5e
Sharmina Akhtar <sakht318@gmail.com>
2019-01-17 19:00:15 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+<<<<<<< HEAD
 
 export default class Spaceship extends React.Component {
   // your code here
@@ -20,6 +21,22 @@ export default class Spaceship extends React.Component {
         {hasRockets}
         {colors}
       </div>
+=======
+const names = [string]
+const speed = [:integer, default => :slow]
+const hasRockets = [:boolean, default => :false]
+const colors = ['black', 'red']
+export default class Spaceship extends React.Component {
+  // your code here
+  render() {
+   return (
+     <div>
+      {this.props.names}
+      {this.props.speed}
+      {this.props.hasRockets}
+      {this.props.colors}
+     </div>
+>>>>>>> dba400912b1b7d56f34a8b1fad0d84e219463f64
 
    )
  }
